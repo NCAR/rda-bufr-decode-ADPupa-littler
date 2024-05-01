fbf8541a811370273ed4a293aa32a49857c77687
Thomas Cram <tcram@ucar.edu>
2024-05-01 17:15:04 -0600

    updated install script

--- install/install.sh
+++ install/install.sh
@@ -9,66 +9,48 @@
 #      gdas.satwnd.tHHz.YYYYMMDD.bufr 
 #      gdas.aircar.tHHz.YYYYMMDD.bufr
 #
-#  bufr_upa2ob.x:       decodes ADPUPA observations and writes to a text file
-#  bufr_aircar2ob.x:    decodes AIRCAR observations and writes to a text file
-#  bufr_craft2ob.x:     decodes AIRCFT observations and writes to a text file
-#  bufr_sat2ob.x:       decodes SATWND observations and writes to a text file
-#  runob2lit_imd_obs.x: reads observation text files and converts to little_r format
-#  dumpbufr.x:          used to dump all contents of a BUFR file.
+#  bufr_adpupa2ob.x: decodes ADPUPA observations and writes to a text file
+#  bufr_aircar2ob.x: decodes AIRCAR observations and writes to a text file
+#  bufr_aircft2ob.x: decodes AIRCFT observations and writes to a text file
+#  bufr_satwnd2ob.x: decodes SATWND observations and writes to a text file
+#  upperair_obs2littler.x: reads observation text files and converts to little_r format
+#
+#  Note: this script assumes this will be run and compiled on a linux 
+#        system with the Gnu Fortran compiler
 #  ------------------------------------------------------------------------
  
 set -eua
  
-#  ------------------------------------------------------------------------
-#  CPLAT - platform type (linux,sgi,aix,sun)
-#  ------------------------------------------------------------------------
- 
-CPLAT=linux
 SRC=../src
-LIB=/path/to/BUFRLIB
+LIB=/path/to/libbufr_4.a
+INCL=/path/to/bufrlib/include/directory
 EXE=../exe
 INSTALL=.
  
-cflag=""
-fflag=""
-
-if [ $CPLAT = linux ]
-then
-   export FC=gfortran
-   export CC=gcc
-   fflag=" -O3 -DUNDERSCORE -fno-second-underscore -w"
-   cflag=" -O3 -DUNDERSCORE -w"
-fi
+export FC=gfortran
+export CC=gcc
+fflag=" -O3 -DUNDERSCORE -fno-second-underscore -w"
+cflag=" -O3 -DUNDERSCORE -w"
 
 #  Compile the decode programs
-#  ---------------------------------------
- 
-echo "Compiling bufr_configdecode_ADPupa programs..."
-$FC $fflag -c $SRC/dumpbufr.f
-
-$FC $fflag -c $SRC/bufr_upa2ob.f
-$FC $fflag -c $SRC/bufr_aircar2ob.f
-$FC $fflag -c $SRC/bufr_craft2ob.f
-$FC $fflag -c $SRC/bufr_sat2ob.f
-
-$FC $fflag -c $SRC/runob2lit_imd_obs.f
+#  --------------------------------------- 
+echo "Compiling bufr decoding programs..."
+$FC $fflag -c $SRC/bufr_adpupa2ob.f -I $INCL
+$FC $fflag -c $SRC/bufr_aircar2ob.f -I $INCL
+$FC $fflag -c $SRC/bufr_aircft2ob.f -I $INCL
+$FC $fflag -c $SRC/bufr_satwnd2ob.f -I $INCL
+$FC $fflag -c $SRC/upperair_obs2littler.f
  
 #  link and load the executables
 #  -----------------------------
-
 echo "Linking..."
-$FC $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB/bufrlib.a
-
-$FC $fflag -o $EXE/bufr_upa2ob.x bufr_upa2ob.o $LIB/bufrlib.a
-$FC $fflag -o $EXE/bufr_aircar2ob.x bufr_aircar2ob.o $LIB/bufrlib.a
-$FC $fflag -o $EXE/bufr_craft2ob.x bufr_craft2ob.o $LIB/bufrlib.a
-$FC $fflag -o $EXE/bufr_sat2ob.x bufr_sat2ob.o $LIB/bufrlib.a
-
-$FC $fflag -o $EXE/runob2lit_imd_obs.x runob2lit_imd_obs.o
+$FC $fflag -o $EXE/bufr_adpupa2ob.x bufr_adpupa2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/bufr_aircar2ob.x bufr_aircar2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/bufr_aircft2ob.x bufr_aircft2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/bufr_satwnd2ob.x bufr_satwnd2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/upperair_obs2littler.x upperair_obs2littler.o
 
 #  clean up
 #  --------
-
-rm -f *.o
-
+rm -f *.o *.mod
 echo "Finished."
